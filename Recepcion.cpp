8121245763647d6601b723f039f2b54f82f7f50a
unknown <romano.mica@gmail.com>
2020-12-04 12:57:10 -0300

    segundo modulo del asistente terminado

--- Recepcion.cpp
+++ Recepcion.cpp
@@ -20,7 +20,7 @@ struct veterinario
 struct fecha
 {
     int dia,mes,anio;
-}
+};
 struct mascota
 {
     char nom[60];
@@ -30,11 +30,20 @@ struct mascota
     fecha nac;
     float pes;
     char tel[25];
-}
+};
+struct turno
+{
+    int mat;
+    fecha fec;
+    int dni;
+    char det[380];
+};
 int buscarUsuario(char usu[10],char cla[10]);
 void registrarMascotas();
 void registrarTurno();
 void listado();
+bool buscarMatricula(int mat);
+bool buscarMascota(int dni);
 
 main()
 {
@@ -81,6 +90,140 @@ main()
 	}
 	getch();	
 	
+}
+bool buscarMatricula(int mat)
+{
+    veterinario reg;
+    bool hallado=false;
+    FILE*arch;
+    arch=fopen("veterinario.dat","rb");
+    fread(&reg,sizeof(reg),1,arch);
+    while(!feof(arch))
+    {
+        if(mat==reg.mat)
+        {
+            hallado=true;
+        }
+        fread(&reg,sizeof(reg),1,arch);
+    }
+    fclose(arch);
+    return hallado;
+}
+bool buscarMascota(int dni)
+{
+    mascota reg;
+    bool hallado=false;
+    FILE*arch;
+    arch=fopen("mascotas.dat","rb");
+    fread(&reg,sizeof(reg),1,arch);
+    while(!feof(arch))
+    {
+        if(dni==reg.dni)
+        {
+            hallado=true;
+        }
+        fread(&reg,sizeof(reg),1,arch);
+    }
+    fclose(arch);
+    return hallado; 
+}
+void registrarMascotas()
+{
+    FILE*arch;
+    mascota reg;
+    printf("\nApellido y Nombre de la mascota: ");
+    _flushall();
+    gets(reg.nom);
+    printf("\nIngrese el domicilio: ");
+    _flushall();
+    gets(reg.dom);
+    printf("\nIngrese el dni: ");
+    scanf("%d", &reg.dni);
+    printf("\nIngrese la localidad: ");
+    _flushall();
+    gets(reg.loc);
+    printf("\nIngrese dia de nacimiento: ");
+    scanf("%d", &reg.nac.dia);
+    printf("\nIngrese mes de nacimiento: ");
+    scanf("%d", &reg.nac.mes);
+    printf("\nIngrese anio de nacimiento: ");
+    scanf("%d", &reg.nac.anio);
+    printf("\nIngrese el peso: ");
+    scanf("%d", &reg.pes);
+    printf("\nIngrese el telefono: ");
+    _flushall();
+    gets(reg.tel);
+    arch=fopen("mascotas.dat", "ab");
+    fwrite(&reg,sizeof(reg),1,arch);
+    fclose(arch);
+    printf("\nLa mascota fue exitosamente registrada: ");
+
+    
+}
+void registrarTurno()
+{
+   FILE*arch;
+   turno reg;
+   bool h1,h2;
+   printf("\nIngrese la matricula del veterinario: "); 
+   scanf("%d", &reg.mat);
+   h1=buscarMatricula(reg.mat);
+   if(h1==false)
+   {
+        printf("\nLa matricula no fue identificada: ");
+   }
+   else
+   {
+       printf("\nIngrese el dni de la mascota: "); 
+       scanf("%d", &reg.dni);
+       h2=buscarMascota(reg.dni);
+       if(h2==false)
+       {
+          printf("\nEl dni no fue hallado: ");  
+       } else{
+                printf("\nIngrese el dia del turno: ");
+                scanf("%d", &reg.fec.dia);
+                printf("\nIngrese el mes del turno: ");
+                scanf("%d", &reg.fec.mes);
+                printf("\nIngrese el anio del turno: ");
+                scanf("%d", &reg.fec.anio);
+                printf("\nIngrese el detalle: ");
+                _flushall();
+                gets(reg.det);
+                arch=fopen("turnos.dat","ab");
+                fwrite(&reg,sizeof(reg),1,arch);
+                fclose(arch);
+                printf("\nTurno exitosamente registrado: ");
+            }
+   }
+}
+void listado()
+{
+    FILE*arch;
+    turno reg;
+    fecha fec;
+    int mat;
+    printf("\nIngrese el dia del turno: ");
+    scanf("%d", &fec.dia);
+    printf("\nIngrese el mes del turno: ");
+    scanf("%d", &fec.mes);
+    printf("\nIngrese el anio del turno: ");
+    scanf("%d", &fec.anio);
+    printf("\nIngrese la matricula del veterinario: ");
+    scanf("%d", &mat);
+    arch=fopen("turnos.dat","rb");
+    fread(&reg, sizeof (reg),1,arch);
+    while(!feof(arch))
+    {
+        if(fec.dia==reg.fec.dia && fec.mes==reg.fec.mes && fec.anio==reg.fec.anio && mat==reg.mat)
+        {
+            printf("\nDni: %d",reg.dni);
+            printf("\nFecha %d/%d/%d: ",reg.fec.dia, reg.fec.mes, reg.fec.anio);
+            printf("\nDetalle: %s", reg.det);
+        }
+        fread(&reg, sizeof (reg),1,arch);
+    }
+    fclose(arch);
 }
 int Buscar(char linea[80],char usu[10],char cla[10])
 {
